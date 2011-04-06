d2369b6a0f54df3cbe310d6b41fe030ee4fcef99
adrynalyne <adrynalyne@gmail.com>
2011-04-05 17:52:32 -0700

    fixed

--- build.sh
+++ build.sh
@@ -3,22 +3,7 @@
 echo "****Start building****"
 echo "****Cleaning****"
 
-rm output/tools/boot.img-kernel
-rm output/tools/out/ramdisk-new.gz
-rm output/files/system/lib/modules/qce.ko
-rm output/files/system/lib/modules/sequans_sdio.ko
-rm output/files/system/lib/modules/qcrypto.ko
-rm output/files/system/lib/modules/qcedev.ko
-rm output/files/system/lib/modules/wimaxdbg.ko
-rm output/files/system/lib/modules/bcm4329.ko
-rm output/files/system/lib/modules/scsi_wait_scan.ko
-rm output/files/system/lib/modules/wimaxuart.ko
-rm output/files/boot.img
-rm output/tools/boot_new.img
-cd output
-rm *.zip
-cd ..
-make clean mrproper
+sh clean.sh
 
 START=$(date +%s)
 echo "****Building****"
@@ -28,10 +13,7 @@ make -j16
 
 echo "****Creating boot image****"
 cp arch/arm/boot/zImage output/tools/boot.img-kernel
-cp drivers/crypto/msm/qce.ko output/files/system/lib/modules/qce.ko
 cp drivers/net/wimax/SQN/sequans_sdio.ko output/files/system/lib/modules/sequans_sdio.ko
-cp drivers/crypto/msm/qcrypto.ko output/files/system/lib/modules/qcrypto.ko
-cp drivers/crypto/msm/qcedev.ko output/files/system/lib/modules/qcedev.ko
 cp drivers/net/wimax/wimaxdbg/wimaxdbg.ko output/files/system/lib/modules/wimaxdbg.ko
 cp drivers/net/wireless/bcm4329_204/bcm4329.ko output/files/system/lib/modules/bcm4329.ko
 cp drivers/scsi/scsi_wait_scan.ko output/files/system/lib/modules/scsi_wait_scan.ko
