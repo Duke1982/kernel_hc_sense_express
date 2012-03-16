975335b63b82bc1fb94285d2ebe84f85bce6d873
James <jmztaylor@gmail.com>
2012-03-16 15:48:50 -0400

    'Cleaned'

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-echo -n "What would you like the commit to say?"
+echo -n "What would you like the commit to say?  "
 read -e COMMIT
 git add .
-git commit -m COMMIT
+git commit -m $COMMIT
 git push
+read -n 1 -p "Press any key to continue"
